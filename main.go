039eff68db808d3fb8fd5df8e22dbee9f918e2b3
Goldmond <sascha.ziebart@me.com>
2026-09-07 22:08:48 +0200

    CH2:L3 Stateful Headers

--- main.go
+++ main.go
@@ -1,34 +1,66 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
-type apiHandler struct{}
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
-//func (apiHandler) ServeHTTP(http.ResponseWriter, *http.Request) {}
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	mux := http.NewServeMux()
-	srv := http.Server{
+	srv := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
-	//add handler for the root path
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
 
-	mux.HandleFunc("/healthz",
-		func(w http.ResponseWriter, req *http.Request) {
-			w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
-			w.WriteHeader(http.StatusOK)
-			io.WriteString(w, "OK\n")
-		},
-	)
+	apiCfg := apiConfig{}
+
+	//add handler for the root path
+	//mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
+	mux.Handle("/app/", http.StripPrefix("/app/", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
+	//mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerReadRequestCount)
+	mux.HandleFunc("/reset", apiCfg.handlerResetCounter)
+
 	//start server
 	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
 		// Error starting or closing listener:
 		log.Fatalf("HTTP server ListenAndServe: %v", err)
 	}
+	log.Println("Serving files from /app on port 8080")
+
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK) //optional, because w.Write() would do it if not called before
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) handlerReadRequestCount(w http.ResponseWriter, r *http.Request) {
+	count := cfg.fileserverHits.Load()
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %d\n", count) //fmt.Fprintf formats and writes directly to w because it implements io.Writer
+}
+
+func (cfg *apiConfig) handlerResetCounter(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits counter reset to 0"))
 }
