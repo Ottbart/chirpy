fd0e411a444935031231f18de8f1e5978367e4f6
Goldmond <sascha.ziebart@me.com>
2026-09-10 21:48:40 +0200

    CH4:L2 JSON

---
+++ handlers.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK) //optional, because w.Write() would do it if not called before
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) handlerReadRequestCount(w http.ResponseWriter, r *http.Request) {
+	count := cfg.fileserverHits.Load()
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	site := fmt.Sprintf(`
+		<html>
+			<body>
+				<h1>Welcome, Chirpy Admin</h1>
+				<p>Chirpy has been visited %d times!</p>
+			</body>
+		</html>`, count)
+	w.Write([]byte(site))
+}
+
+func (cfg *apiConfig) handlerResetCounter(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits counter reset to 0"))
+}
+
+func (cfg *apiConfig) handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	type Chirp struct {
+		Body  string `json:"body"`
+		Valid bool   `json:"valid"`
+	}
+	// decode json from request
+	decoder := json.NewDecoder(r.Body)
+	chirp := Chirp{}
+	err := decoder.Decode(&chirp)
+	if err != nil {
+		log.Printf("error decoding chirp: %v", err)
+		err = respondWithError(w, 500, "error decoding chirp")
+		if err != nil {
+			log.Printf("error sending error-response: %v", err)
+		}
+	}
+
+	//check if chirp has max 140 characters
+	if len(chirp.Body) > 140 {
+		err = respondWithError(w, 400, "Chirp is too long")
+		if err != nil {
+			log.Printf("error sending error-response: %v", err)
+		}
+		return
+	}
+
+	//response OK
+	err = respondWithJSON(w, 200, Chirp{Valid: true})
+	if err != nil {
+		log.Printf("error sending response: %v", err)
+	}
+}
--- main.go
+++ main.go
@@ -1,22 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"sync/atomic"
 )
-
-type apiConfig struct {
-	fileserverHits atomic.Int32
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
 
 func main() {
 	mux := http.NewServeMux()
@@ -32,35 +19,15 @@ func main() {
 	mux.Handle("/app/", http.StripPrefix("/app/", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
 	//mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
 
-	mux.HandleFunc("/healthz", handlerReadiness)
-	mux.HandleFunc("/metrics", apiCfg.handlerReadRequestCount)
-	mux.HandleFunc("/reset", apiCfg.handlerResetCounter)
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerReadRequestCount)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerResetCounter)
+	mux.HandleFunc("POST /api/validate_chirp", apiCfg.handlerValidateChirp)
 
 	//start server
+	log.Println("Serving files from / on port 8080")
 	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
 		// Error starting or closing listener:
 		log.Fatalf("HTTP server ListenAndServe: %v", err)
 	}
-	log.Println("Serving files from /app on port 8080")
-
-}
-
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK) //optional, because w.Write() would do it if not called before
-	w.Write([]byte(http.StatusText(http.StatusOK)))
-}
-
-func (cfg *apiConfig) handlerReadRequestCount(w http.ResponseWriter, r *http.Request) {
-	count := cfg.fileserverHits.Load()
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Hits: %d\n", count) //fmt.Fprintf formats and writes directly to w because it implements io.Writer
-}
-
-func (cfg *apiConfig) handlerResetCounter(w http.ResponseWriter, r *http.Request) {
-	cfg.fileserverHits.Store(0)
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hits counter reset to 0"))
 }
